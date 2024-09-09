654ae942ee5aa5a826a3a4417a252242950fe4c4
廿一日的船 <aprosail@outlook.com>
2024-09-09 10:27:11 +0800

    测试Lint规则的格式化文本输出

--- strict-lints/spider/rules_test.go
+++ strict-lints/spider/rules_test.go
@@ -8,9 +8,12 @@ import (
 
 func TestRuleFmt(t *testing.T) {
 	rule := spider.LintRule{
-		Name: "test_rule",
+		Name:    "test_rule",
+		Status:  spider.STATUS_DEPRECATED,
+		HasFix:  true,
+		Flutter: true,
 	}
-	const result = "test_rule()"
+	const result = "test_rule(deprecated,fix,flutter)"
 	if rule.String() != result {
 		t.Errorf("Expected %s, got %s", result, rule.String())
 	}
